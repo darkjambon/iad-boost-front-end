2236052b5270c8a7dea554321e21749531cb7097
Yann Dias de Novaes <yann.dias-de-novaes@afp-ext.com>
2025-05-30 18:08:11 +0200

    force crash vercel

--- entry.vercel-edge.ts
+++ entry.vercel-edge.ts
@@ -1,3 +1,12 @@
 import { createVercelEdgeHandler } from '@analogjs/platform/vercel';
 
-export default createVercelEdgeHandler();
+export default createVercelEdgeHandler({
+  onError(error) {
+    console.error('SSR error:', error);
+    return new Response('SSR Error: ' + (error?.message || error), { status: 500 });
+  },
+  async onRequest(context) {
+    console.log('SSR: Edge handler running!');
+    // return new Response('OK', { status: 200 }); // décommente pour voir une page "OK"
+  }
+});
